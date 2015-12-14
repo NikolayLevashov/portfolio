118a908d31cb1888bc9d7b690304172bbea500d2
NikolayLevashov <graflevashov@gmail.com>
2015-12-14 18:26:12 +0300

    v.0.1

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+	$('#content-block').find('a').click(function(){
+
+		var index = $(this).index();
+
+		$('body').addClass('static');
+
+		setTimeout(function(){
+			$('#content-block').removeClass('mainpage');
+		}, 1000);
+
+		console.log ( index )
+
+		$('#content-block').find('.info-block').removeClass('active');
+		$('#content-block').find('.info-block').eq(index).addClass('active');
+		
+	});
 
 });
